007a01739244c69162a66ff0556c58d6144f69c3
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-17 14:23:43 +0530

    [Raja]Add Retrieving the data based on thier state from the table implemented in AddressBook

--- AddressBook.sql
+++ AddressBook.sql
@@ -15,5 +15,15 @@ Zip bigint
 INSERT INTO Address_Book(FirstName,LastName,Email,Phnum,Address,State,Zip)values('Nari','Guggilla','guggillanarendar@gmail.com',9837782464,'VJW','Andhra',247473);
 SELECT * FROM Address_Book
 SELECT* FROM Address_Book where FirstName='Nari';
+
 /* UC-4 Edit the addressbook of existing contact based on their names */
 UPDATE Address_Book SET FirstName = 'Nagi'WHERE FirstName = 'Nari';
+
+/* UC-5 Ability to delete the person */
+delete from Address_Book where FirstName='Nagi';
+
+/*UC-6 Ability to retrive the data based on thier city */
+SELECT* FROM Address_Book where State='Telangana';
+SELECT* FROM Address_Book where State='VJW';
+
+
