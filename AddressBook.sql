dd45dffc727940eab2982b1040b6b4b4b8c0815d
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-17 14:13:02 +0530

    [Raja]Add Editing thr data the data into the table implemented in AddressBook

--- AddressBook.sql
+++ AddressBook.sql
@@ -11,3 +11,9 @@ Address varchar(200),
 State varchar(200),
 Zip bigint
 );
+/* Inserting the data into the Table */
+INSERT INTO Address_Book(FirstName,LastName,Email,Phnum,Address,State,Zip)values('Nari','Guggilla','guggillanarendar@gmail.com',9837782464,'VJW','Andhra',247473);
+SELECT * FROM Address_Book
+SELECT* FROM Address_Book where FirstName='Nari';
+/* UC-4 Edit the addressbook of existing contact based on their names */
+UPDATE Address_Book SET FirstName = 'Nagi'WHERE FirstName = 'Nari';
