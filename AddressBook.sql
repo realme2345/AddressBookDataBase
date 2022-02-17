b5e76947385b67b7bc54034522ea0a9c775c76a0
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-17 13:38:41 +0530

    [Raja]Add Creating table data implemented in AddressBook

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,13 @@
 /* Create address book data base */
 create database AddressBook
+/* Create Table address book */
+create table Address_Book(
+Id int IDENTITY(1,1) PRIMARY KEY,
+FirstName varchar(200),
+LastName varchar(200),
+Email varchar(200),
+Phnum bigint,
+Address varchar(200),
+State varchar(200),
+Zip bigint
+);
